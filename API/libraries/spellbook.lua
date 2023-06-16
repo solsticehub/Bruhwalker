79891af5aad2d86a59ef38ba121d8e1a794b8bd9
solsticehub <107598962+solsticehub@users.noreply.github.com>
2023-06-16 11:59:14 +0200

    Update spellbook.lua

--- API/libraries/spellbook.lua
+++ API/libraries/spellbook.lua
@@ -7,19 +7,19 @@ function spellbook:get_spell_slot(spell_slot)
 end
 
 ---@param spell_slot any
----@param cast_delay number
----@param x number|vec3
----@param y number|vec3
----@param z number|vec3
----@param x1 number|vec3
----@param y1 number|vec3
----@param z1 number|vec3
+---@param cast_delay? number
+---@param x? number|vec3
+---@param y? number|vec3
+---@param z? number|vec3
+---@param x1? number|vec3
+---@param y1? number|vec3
+---@param z1? number|vec3
 function spellbook:cast_spell(spell_slot, cast_delay, x, y, z, x1, y1, z1)
 end
 
 ---@param spell_slot any
 ---@param target gameObject
----@param cast_delay number
+---@param cast_delay? number
 function spellbook:cast_spell_targetted(spell_slot, target, cast_delay)
 end
 
